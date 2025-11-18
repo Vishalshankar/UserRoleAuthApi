af618e818b235805448e375ee211f02cced80ada
vishal shankar <vishal119.sh14@gmail.com>
2025-11-18 22:55:15 +0530

    feat(core): add ApplicationUser, ApplicationRole and DTOs

---
+++ UserRoleAuth.Core/DTOs/AuthDtos.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserRoleAuth.Core.DTOs
+{
+    internal class AuthDtos
+    {
+        public class CreateUserDto
+        {
+            public string UserName { get; set; } = null!;
+            public string Email { get; set; } = null!;
+            public string Password { get; set; } = null!;
+            public string? DisplayName { get; set; }
+        }
+
+        public class LoginDto
+        {
+            public string UserName { get; set; } = null!;
+            public string Password { get; set; } = null!;
+        }
+
+        public class TokenResponseDto
+        {
+            public string AccessToken { get; set; } = null!;
+            public string RefreshToken { get; set; } = null!;
+            public DateTime ExpiresAt { get; set; }
+        }
+    }
+}
---
+++ UserRoleAuth.Core/Entities/RefreshToken.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserRoleAuth.Core.Entities
+{
+    internal class RefreshToken
+    {
+        public int Id { get; set; }
+        public string Token { get; set; } = null!;
+        public string UserId { get; set; } = null!;
+        public DateTime Expires { get; set; }
+        public bool IsRevoked { get; set; }
+        public DateTime Created { get; set; } = DateTime.UtcNow;
+    }
+}
