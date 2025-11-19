f4296e90fd5e51616da42ba257051a2f54e40955
vishal shankar <vishal119.sh14@gmail.com>
2025-11-19 15:16:14 +0530

    add report repository and JWT Service

--- UserRoleAuth.Infrastructure/Repositories/ReportRepository.cs
+++ UserRoleAuth.Infrastructure/Repositories/ReportRepository.cs
@@ -3,10 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
 
 namespace UserRoleAuth.Infrastructure.Repositories
 {
-    internal class ReportRepository
+    public class ReportRepository
     {
+        private readonly IConfiguration _config;
+        public ReportRepository(IConfiguration config) => _config = config;
+
+        public async Task<int> GetTotalUsersAsync()
+        {
+            var connStr = _config.GetConnectionString("DefaultConnection");
+            await using var conn = new SqlConnection(connStr);
+            await using var cmd = new SqlCommand("SELECT COUNT(*) FROM AspNetUsers", conn);
+            await conn.OpenAsync();
+            var res = await cmd.ExecuteScalarAsync();
+            return Convert.ToInt32(res);
+        }
     }
 }
--- UserRoleAuth.Infrastructure/Services/JwtService.cs
+++ UserRoleAuth.Infrastructure/Services/JwtService.cs
@@ -3,10 +3,59 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UserRoleAuth.Core.Entities;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using UserRoleAuth.Core.Entities;
+using System.Security.Cryptography;
 
 namespace UserRoleAuth.Infrastructure.Services
 {
-    internal class JwtService
+    public interface IJwtService
     {
+        string GenerateAccessToken(ApplicationUser user, IEnumerable<string> roles);
+        string GenerateRefreshToken();
+    }
+    public class JwtService : IJwtService
+    {
+        private readonly IConfiguration _config;
+        public JwtService(IConfiguration config) => _config = config;
+
+        public string GenerateAccessToken(ApplicationUser user, IEnumerable<string> roles)
+        {
+            var key = Encoding.UTF8.GetBytes(_config["Jwt:Key"]);
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.NameIdentifier, user.Id),
+                new Claim(ClaimTypes.Name, user.UserName ?? ""),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+            };
+
+            foreach (var r in roles)
+                claims.Add(new Claim(ClaimTypes.Role, r));
+
+            var creds = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256);
+            var expires = DateTime.UtcNow.AddMinutes(double.Parse(_config["Jwt:AccessTokenMinutes"] ?? "120"));
+
+            var token = new JwtSecurityToken(
+                issuer: _config["Jwt:Issuer"],
+                audience: _config["Jwt:Audience"],
+                claims: claims,
+                expires: expires,
+                signingCredentials: creds
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+        public string GenerateRefreshToken()
+        {
+            var random = new byte[32];
+            using var rng = RandomNumberGenerator.Create();
+            rng.GetBytes(random);
+            return Convert.ToBase64String(random);
+        }
     }
 }
